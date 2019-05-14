3e287f466a6e032e4beb9d51bc20cdee39387b3e
Inyong Jung <jiy8319@gmail.com>
2019-05-14 21:05:26 +0900

    setting init

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+router.use('/user',function(){
+
+});
+
+router.use('/menu',function(){
+
+});
+
+router.use('/store', function(){
+
+});
+
+
+module.exports = router;
---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('./router');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(router);
+
+app.listen(3000,'localhost',()=>{
+  console.log('Start Server');
+});
+
