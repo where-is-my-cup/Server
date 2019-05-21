17578934531169ec6bc0c7d2d0d0d104da2ca879
kimjounguk <dnrl24a@gmail.com>
2019-05-21 21:23:21 +0900

    store/server

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,10 +1,15 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const user = sequelize.define('user', {
-    loginId: DataTypes.STRING,
-    password: DataTypes.STRING,
-    nicname: DataTypes.STRING
-  }, {});
+  const user = sequelize.define(
+    "user",
+    {
+      loginId: DataTypes.STRING,
+      password: DataTypes.STRING,
+      nickname: DataTypes.STRING,
+      admin: DataTypes.STRING
+    },
+    {}
+  );
   user.associate = function(models) {
     // associations can be defined here
   };
--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const menuRouter = require("./menu");
 const router = express.Router();
 const user = require("./user");
+const store = require("./store");
 
 router.use("/user", user);
-router.use("/store", function() {});
+router.use("/store", store);
 router.use("/menu", menuRouter);
 
 module.exports = router;
