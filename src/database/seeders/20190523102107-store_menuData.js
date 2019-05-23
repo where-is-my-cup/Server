23f4278b1bde0c305f375e87abe3ab335cf8e89c
kimjounguk <dnrl24a@gmail.com>
2019-05-23 23:42:08 +0900

    store/websoket2

---
+++ src/database/seeders/20190523102107-store_menuData.js
@@ -0,0 +1,782 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "store_menus",
+      [
+        {
+          store_id: 1,
+          menu_id: 1,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 2,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 3,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 4,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 5,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 6,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 7,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 8,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 9,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 10,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 11,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 12,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 13,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 14,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 15,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 16,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 17,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 18,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 1,
+          menu_id: 19,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 1,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 2,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 3,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 4,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 5,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 6,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 7,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 8,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 9,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 10,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 11,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 12,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 13,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 14,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 15,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 16,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 17,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 18,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 2,
+          menu_id: 19,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 1,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 2,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 3,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 4,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 5,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 6,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 7,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 8,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 9,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 10,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 11,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 12,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 13,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 14,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 15,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 16,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 17,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 18,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 3,
+          menu_id: 19,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 1,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 2,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 3,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 4,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 5,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 6,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 7,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 8,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 9,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 10,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 11,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 12,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 13,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 14,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 15,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 16,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 17,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 18,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 4,
+          menu_id: 19,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 1,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 2,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 3,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 4,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 5,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 6,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 7,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 8,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 9,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 10,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 11,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 12,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 13,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 14,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 15,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 16,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 17,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 18,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        },
+        {
+          store_id: 5,
+          menu_id: 19,
+          price: 5000,
+          count: 99,
+          createdAt: "2019-05-23",
+          updatedAt: "2019-05-23"
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
